f1144f23554fff78d57cde6d29f204e5059fb3be
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-30 11:36:08 +0530

    paras

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ app.get('/api/products',(req,res) => {
     res.json(newProducts)
 })
 
-app.get('/api/products/1',(req,res) => {
-    const singleProduct = products.find((product) => product.id===1)
+app.get('/api/products/:productID',(req,res) => {
+    const {productID} = req.params;
+    const singleProduct = products.find((product) => product.id===Number(productID))
+    if(!singleProduct)
+    {
+        return res.status(404).send('Product not found')
+    }
     res.json(singleProduct)
 })
 
