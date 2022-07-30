49f0fcb3b9996ce5395bcdc0c4a81a15f946cc17
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-30 10:05:40 +0530

    json basics

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express')
-const path = require('path')
 const app = express()
 
-app.use(express.static('./public'))
-
 app.get('/',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+    res.json([{name:'john'},{name:'susan'}])
 })
 
-app.all('*',(req,res) => {
-    res.status(404).send('Page not found')
-})
-app.listen(3080,()=>{
-    console.log('Server listening to port 3080')
+app.listen(3080,() => {
+    console.log("Server listening to port 3080...")
 })
