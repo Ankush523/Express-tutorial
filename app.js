d46d274c5082ec39d3bb73ab615bf10551286c66
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-30 10:39:21 +0530

    api

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
-
+const {products} = require('./data')
 app.get('/',(req,res) => {
-    res.json([{name:'john'},{name:'susan'}])
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+
+app.get('/api/products',(req,res) => {
+    const newProducts = products.map((product)=>{
+        const{id,name,image} = product;
+        return{id,name,image}; 
+    })
+    res.json(newProducts)
 })
 
 app.listen(3080,() => {
