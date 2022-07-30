87937aa2ce606655a124ea49aedeb7d1ae043548
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-07-30 09:02:35 +0530

    express app

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.use(express.static('./public'))
+
 app.get('/',(req,res) => {
-    res.send('Home Page') 
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
 })
 
-app.get('/about',(req,res) => {
-    res.send('About Page')
+app.all('*',(req,res) => {
+    res.status(404).send('Page not found')
 })
-
-app.listen(8000,()=>{
-    console.log('Server listening to port 8000')
+app.listen(3080,()=>{
+    console.log('Server listening to port 3080')
 })
