662bb9843787adc6a86467aa089379907727036a
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-01 14:47:27 +0530

    express routers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-let { people } = require('./data')
-
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 // static assets
 app.use(express.static('./methods-public'))
 // parse form data
@@ -9,60 +9,10 @@ app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
 
-app.get('/api/people', (req, res) => {
-  res.status(200).json({ success: true, data: people })
-})
+app.use('/api/people',people)
 
-app.post('/api/people', (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'please provide name value' })
-  }
-  res.status(201).json({ success: true, person: name })
-})
+app.use('/login',auth)
 
-app.post('/api/postman/people', (req,res) => {
-    const {name} = req.body
-    if (!name) {
-        return res
-          .status(400)
-          .json({ success: false, msg: 'please provide name value' })
-      }
-      res.status(201).json({ success: true, data : [...people,name] })
-})
-
-app.post('/login', (req, res) => {
-    const { name } = req.body
-    if (name) {
-      return res.status(200).send(`Welcome ${name}`)
-    }
-  
-    res.status(401).send('Please Provide Credentials')
-  })
-
-app.put('/api/people/:id', (req,res) => {
-    const {id} = req.params
-    const {name} = req.body
-    const person = people.find((person) => person.id === Number(id))
-
-    if (!person) {
-        return res
-          .status(404)
-          .json({ success: false, msg:`no person with id ${id}` })
-    }
-    const newPeople = people.map((person) => {
-        if(person.id === Number(id))
-        {
-            person.name = name
-        }
-        return person
-    })
-    res.status(200).json({success : true, data :newPeople})
-  })
-
-  
 app.listen(3000,()=>{  
     console.log('Server listening to port 3000...')
 })
