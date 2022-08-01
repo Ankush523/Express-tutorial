e31520c59bc7aad47322ce2751963674c0e5f2c6
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-01 11:03:49 +0530

    post method

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
-app.use([logger,authorize])
+let {people} = require('./data')
 
-app.get('/',logger,(req,res) => {
-    res.send('Home')
+app.use(express.static('./methods-public'))
+app.get('./api/people' , (req,res) => {
+    res.status(200).json({success:true , data:people})
 })
 
-app.get('/about',logger,(req,res) => {
-    res.send('About')
+app.post('/login' , (req,res) => {
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`welcome ${name}`)
+    }
+    else{
+        return res.status(404).send("Please provie valid credentials ")
+    }
+    res.send("post")
 })
 app.listen(3080,()=>{  
     console.log('Server listening to port 3080...')
